78a54b0a0538572fb2dcf6a14eb2d78128e5e34f
liuding <liuding@ifmuse.com>
2017-03-08 19:48:26 +0800

    update file

---
+++ src/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <transition name="path-fade" mode="out-in">
+        <!-- 路由匹配到的组件将渲染在这里 -->
+        <router-view></router-view>
+      </transition>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: #fbfbfb ;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+
+.path-fade-enter-active, .path-fade-leave-active {
+      transition: opacity .3s;
+  }
+.path-fade-enter, .path-fade-leave {
+    opacity: 0;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,12 @@
-export default {
-  '/home': 'Home',
-  '/list': 'List'
-}
+import VueRouter from "vue-router" ;
+import Home from "./pages/home.vue" ;
+import List from "./pages/list.vue" ;
+
+export default new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/home' , component: Home },
+    { path: '/list' , component: List }
+  ]
+})
