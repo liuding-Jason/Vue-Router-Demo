e8cf0a7b65cacc393a6d21c182950f23e0222145
liuding <liuding@ifmuse.com>
2017-03-21 18:01:20 +0800

    update karma test

--- karma.conf.js
+++ karma.conf.js
@@ -26,12 +26,16 @@ module.exports = function(config) {
         } ,
         vue : {
             loaders : {
-                js : 'babel'
+                js : 'isparta-loader'
             }
         } ,
-        babel : {
-            presets : ['es2015']
-        }
+        isparta : {
+            embedSource : true ,
+            noAutoWrap : true ,
+            babel : {
+                presets : ['es2015']
+            }
+        } 
     },
 
     // frameworks to use
@@ -60,7 +64,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress' , 'coverage'],
 
 
     // web server port
@@ -77,7 +81,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
