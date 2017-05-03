e2aa8ce65741acb4b1ec4a7458cb897e0ded9ddd
liuding <liuding@ifmuse.com>
2017-05-03 18:39:52 +0800

    update package

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("server");
+
+
+var express = require("express") ;
+var app = express() ;
+
+app.get("/" , function(req , res){
+	res.status(200);
+	res.send("<h1>Hello Express!</h1>");
+});
+
+app.listen(3003);
+console.log("server is running at port 3000");
